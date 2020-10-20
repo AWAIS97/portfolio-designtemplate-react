3123d52354b9c5361c36c0b60fc745c8009dbbf7
Awais Hassan <55058852+AWAIS97@users.noreply.github.com>
2020-10-20 14:14:10 +0500

    Type animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typing from 'react-typing-animation';
 
 function Main() {
     return (
@@ -6,11 +7,14 @@ function Main() {
       <div className="hero__wrapper">
         <div className="column__left">
           <span>01 - Awais Hassan</span>
+          <Typing>
           <h1>FrontEnd Web Developer</h1>
+          </Typing>
           <p>
             I have years of experience working with small and large companies
             and I'm currently looking for the next opportunity.
           </p>
+          
         </div>
         <div className="column__right">
           <img src={require("./images/web_developer.svg")} alt="brain" className="hero__img" />
